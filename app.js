5d71257086aa4a7b278c908cc51b56989ebffca2
Vikas Kharkwal <weyvik@github.com>
2021-04-02 16:50:53 +0530

    fixed some logic and design

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const userScoreCard = document.querySelector(".score .user");
 const compScoreCard = document.querySelector(".score .comp");
-const rockButton = document.querySelector(".options .rock");
-const paperButton = document.querySelector(".options .paper");
-const scissorsButton = document.querySelector(".options .scissors");
+const rockButton = document.querySelector(".options .r");
+const paperButton = document.querySelector(".options .p");
+const scissorsButton = document.querySelector(".options .s");
 const status = document.querySelector(".status");
 
 let userScore = 0;
@@ -16,47 +16,52 @@ let compChoice = randChoice();
 main();
 
 function randChoice() {
-	return Math.floor(Math.random() * 3) + 1;
+	switch (Math.floor(Math.random() * 3) + 1) {
+		case 1:
+			return "r";
+		case 2:
+			return "p";
+		case 3:
+			return "s";
+	}
 };
 
 function main() {
 	rockButton.addEventListener("click", () => {
-		userChoice = 1;
-		rockButton.classList.add("active-selection");
+		userChoice = "r";
 		checkDecision(userChoice);
-		setTimeout(() => { rockButton.classList.remove("active-selection"); }, 600);
 		compChoice = randChoice();
 	});
 
 	paperButton.addEventListener("click", () => {
-		userChoice = 2;
-		paperButton.classList.add("active-selection");
+		userChoice = "p";
 		checkDecision(userChoice);
-		setTimeout(() => { paperButton.classList.remove("active-selection"); }, 600);
 		compChoice = randChoice();
 	});
 
 	scissorsButton.addEventListener("click", () => {
-		userChoice = 3;
-		scissorsButton.classList.add("active-selection");
+		userChoice = "s";
 		checkDecision(userChoice);
-		setTimeout(() => { scissorsButton.classList.remove("active-selection"); }, 600);
 		compChoice = randChoice();
 	});
 };
 
 function checkDecision(userChoice) {
-	switch (userChoice - compChoice) {
-		case -1:
-		case 2:
-			loss(compChoice);
+	switch (userChoice + compChoice) {
+		case "rs":
+		case "pr":
+		case "sp":
+			win(userChoice);
 			break;
-		case 0:
+		case "rr":
+		case "pp":
+		case "ss":
 			draw(userChoice);
 			break;
-		case 1:
-		case -2:
-			win(userChoice);
+		case "rp":
+		case "sr":
+		case "ps":
+			loss(compChoice);
 			break;
 	}
 	userScoreCard.innerHTML = userScore;
@@ -66,49 +71,56 @@ function checkDecision(userChoice) {
 function win(user) {
 	userScore++;
 	switch (user) {
-		case 1:
+		case "r":
 			statusString = "Rock crushed Scissors. ";
 			break;
-		case 2:
+		case "p":
 			statusString = "Paper covered Rocks. ";
 			break;
-		case 3:
+		case "s":
 			statusString = "Scissors sliced Paper. ";
 			break;
 	}
 	status.innerHTML = statusString + "<strong>You</strong> win.";
+
+	document.querySelector(`.${userChoice}`).classList.add("win");
+	setTimeout(() => document.querySelector(`.${userChoice}`).classList.remove("win"), 600)
 };
 
 function loss(comp) {
 	compScore++;
 	switch (comp) {
-		case 1:
+		case "r":
 			statusString = "Rock crushed Scissors. ";
 			break;
-		case 2:
+		case "p":
 			statusString = "Paper covered Rocks. ";
 			break;
-		case 3:
+		case "s":
 			statusString = "Scissors sliced Paper. ";
 			break;
 	}
 	status.innerHTML = statusString + "<strong>Comp</strong> wins.";
+	document.querySelector(`.${userChoice}`).classList.add("loss");
+	setTimeout(() => document.querySelector(`.${userChoice}`).classList.remove("loss"), 600)
 
 };
 
 function draw(user) {
 	switch (user) {
-		case 1:
+		case "r":
 			statusString = "Two Rocks collided. ";
 			break;
-		case 2:
+		case "p":
 			statusString = "Two Papers stacked. ";
 			break;
-		case 3:
+		case "s":
 			statusString = "Two Scissors crossed.";
 			break;
 	}
 	status.innerHTML = statusString + "<strong>Draw</strong>.";
+	document.querySelector(`.${userChoice}`).classList.add("draw");
+	setTimeout(() => document.querySelector(`.${userChoice}`).classList.remove("draw"), 600)
 };
 
 
